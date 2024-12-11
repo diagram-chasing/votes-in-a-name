45c299fa613ab65073814e33dd69a58196f3c866
thedivtagguy <amanbhargava2001@gmail.com>
2024-12-11 13:04:19 +0530

    feat: add pudding-cup footer

---
+++ src/lib/components/primary/Award.svelte
@@ -0,0 +1,15 @@
+<script>
+	import PuddingCup from '$assets/a-win.webp';
+</script>
+
+<div class="flex flex-col gap-4 mx-auto">
+	<a href="https://pudding.cool/pudding-cup/"
+		><img class="w-32" src={PuddingCup} alt="This project won the 2024 Pudding Cup" /></a
+	>
+</div>
+
+<style>
+	img {
+		width: 300px;
+	}
+</style>
